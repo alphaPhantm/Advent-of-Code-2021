32f169d849e16f1e1b1f1457a4290effa5616965
Noah Kessinger <n_kess@outlook.de>
2021-12-09 14:47:51 +0100

    Day 8 Update

--- 8/8.py
+++ 8/8.py
@@ -1,23 +1,31 @@
 with open("8data.in") as d:
     raw_data = d.read().strip().split("\n")
 
-input_data = [[sorted(line[:line.index("|") - 1].split(" ")), line[line.index("|") + 2:].split(" ")] for line in raw_data]
+input_data = [[line[:line.index("|") - 1].split(" "), line[line.index("|") + 2:].split(" ")] for line in raw_data]
 
-digits_key = [
-    "abcefg",
-    "cf",
-    "acdeg",
-    "acdfg",
-    "bcdf",
-    "abdfg",
-    "abdefg",
-    "acf",
-    "abcdefg",
-    "abcdfg"
-]
-digits = tuple(sorted(digits_key))
 
-for line in input_data:
-    pass
+for i in range(len(input_data)):
+    map = {
+        0: "",
+        1: "",
+        2: "",
+        3: "",
+        4: "",
+        5: "",
+        6: "",
+        7: "",
+        8: "",
+        9: "",
+    }
+    for y in range(len(input_data[i][0])):
+        if len(input_data[i][0][y]) == 2:
+            map[1] = input_data[i][0][y]
+        elif len(input_data[i][0][y]) == 3:
+            map[7] = input_data[i][0][y]
+        elif len(input_data[i][0][y]) == 4:
+            map[7] = input_data[i][0][y]
+        elif len(input_data[i][0][y]) == 7:
+            map[8] = input_data[i][0][y]
 
-print("")
+
+
